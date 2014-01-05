f74d7f093c527631f84c52493ed2d1473496f3bf
kevingimbel <hallo@kevingimbel.com>
2014-01-05 19:29:35 +0100

    re-wrote JavaScript loading so it loads after the DOM and CSS are loaded as suggested by Tim

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-   // '- waiting for the DOM to finish loading, equal to $(document).ready() in jQuery
-
 // replace function by Olly (@0x04)
 function replace(result) {
   for (var index = 1, length = arguments.length; index < length; index++)
@@ -84,5 +81,12 @@ function replace(result) {
     };
 
     // sending data
-    xml.send();    
-});
+    xml.send();
+
+    // Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-46322057-1', 'bullg.it');
+ga('send', 'pageview');
