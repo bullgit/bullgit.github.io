43c11cb0393b9378f15379c44ac351664b732650
kevingimbel <hallo@kevingimbel.com>
2014-08-27 10:38:59 +0200

    viva la bullhead!

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,12 +1,3 @@
-// replace function by Olly (@0x04)
-function replace(result) {
-  for (var index = 1, length = arguments.length; index < length; index++)
-  {
-    result = result.replace('%s', arguments[index]);
-  }
-  
-  return result;
-}
     // The loader element
     var loader = document.querySelector("[data-js='loader']");
 
@@ -14,7 +5,7 @@ function replace(result) {
     var xml = new XMLHttpRequest();
     
     // opening a connection
-    xml.open('GET','https://api.github.com/orgs/bullgit/repos', true);
+    xml.open('GET','https://api.github.com/orgs/bullgit/repos?per_page=100', true);
     
 
 
@@ -25,67 +16,79 @@ function replace(result) {
           // removing the loader after we got the repos
           document.querySelector("[data-js=featured-project]").removeChild(loader);
         // controller for the grid system
-        var j;
+        var j = 0;
         // we take the response
         var res = xml.responseText;
         // put it into a new var and parse it 
-        var obj = JSON.parse(res);
+        var data = JSON.parse(res);
 
-        
-        // then run over ALL THE BULLGIT
-        // running reverse because github goes from oldest to newest
-        for(i = obj.length - 1; i >= 0; i-- ) {
+        // template for featured Project (latest)
+        var templateFeatured = '<article class="content--wrap  featured--article">' +
+        '<h2>' +
+          '<a href="{html_url}">{name}</a>' + 
+        '</h2>' +
+          '<p>{description}</p>'+
+        '</article>';
 
-            var url = (obj[i]['homepage'] === null || obj[i]['homepage'] === '' )
-                      ? 'https://github.com/' + obj[i]['full_name'] 
-                      : obj[i]['homepage']; 
+        // template for all other projects
+        var template = '<article class="project">' +
+        '<h3 class="project--headline">' +
+          '<a href="{html_url}">{name}</a>' + 
+        '</h3>' +
+          '<p>{description}</p>'+
+        '</article>';
 
-            // if it's the first item
-            if(i === obj.length - 1) {
-                // create a layout for the first featured project
-            var layout = "<article class='content--wrap  featured--article'>" +
-                         "<h2>" +
-                            replace('<a href="%s">%s</a>', url, obj[i]['name']) + 
-                        "</h2>" +
-                            replace('<p>%s</p>', obj[i]['description']) +
-                         "</article>";
+        // going through all the received data; reversed because they come oldest to newest
+        for(var i = data.length - 1; i >= 0; i--) {
+          // asigning var obj to the current repo we're dealing with
+          var obj = data[i];
 
-                output = document.querySelector("[data-js=featured-project]"); 
+          // if the grid controller var is 0 (first time we go through this)
+          if(j == 0) {
+            // use the featured layout
+            var layout = templateFeatured.replace(/\{(.*?)\}/g, function(match, property) {
+              return obj[property];
+            });
+            // if j is not 0 
+        } else {
+            // use the normal project template
+            var layout = template.replace(/\{(.*?)\}/g, function(match, property) {
+              return obj[property];
+            });          
+        }
 
-                var j = 1;               
-            } else {
-
-              // output = document.querySelector("[data-js=old-projects]");
-
-            // create a layout for all other projects
-            var layout = "<article class='project'>" +
-                         "<h3 class='project--headline'>" +
-                            replace('<a href="%s">%s</a>', url, obj[i]['name']) + 
-                        "</h3>" +
-                             "<p>"+ obj[i]['description'] + "</p>" +
-                         "</article>";
-
-                // select an output @TODO[Kevin]: Re-think logic...
+          // same as above - defines where to insert the data.
+          // if j IS 0, we insert into the featured project section
+          if(j === 0) {
+            output = document.querySelector("[data-js=featured-project]");
+            j++; // increasing j
+            // if j is not 0, we the value with a switch statement
+          } else {
               switch(j) {
+                // in case j IS 1, we insert the data into column 1
                 case 1:
                   var output = document.querySelector("[data-column='1']");
-                  j++;
-                  break;
+                  j++; // and increase j by 1
+                  break; // and break here so we can return to top and take the next data package
+                // in case j IS 2, we insert the data into column 2
                 case 2:
                   var output = document.querySelector("[data-column='2']");
-                  j = 1;
+                  j = 1; // and set j back to 1 so the next pack of data comes into column 1
                   break;
-                default:
+                default: // by default j IS 1
                   j = 1;
               }  
-            }       
-            // and adding our brand-new bullshit to it.
-            output.innerHTML += layout;
-        }
+            } 
+
+          // inserting the layout into the output var
+          output.innerHTML += layout;
+        } // end of the for loop
+
     } else {
+      // if Github doesn't respond or there's something wrong, we display it inside the loader div
       loader.innerHTML = "Unable to fetch repos from Github.";
     }
-  }
+  } // end of onreadystatechange
 
     // sending data
     xml.send();
