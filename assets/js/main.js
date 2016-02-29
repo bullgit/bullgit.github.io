842f11025cf2a8d34b5e95e02ac32a943e48fe6e
Haroen Viaene <hello@haroen.me>
2016-02-29 11:29:07 +0100

    add search

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,19 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
+
+window.addEventListener('DOMContentLoaded',function(){
+  var search = document.querySelector('.search input');
+  var courseNames = document.querySelectorAll('project--headline a');
+
+  search.addEventListener('input',function(){
+    for (var i = 0; i < courseNames.length; i++) {
+      if (courseNames[i].innerHTML.toLowerCase().indexOf(search.value.toLowerCase()) === -1) {
+        courseNames[i].parentNode.parentNode.classList.add('hidden');
+      } else {
+        courseNames[i].parentNode.parentNode.classList.remove('hidden');
+      }
+    };
+  });
+});
+
